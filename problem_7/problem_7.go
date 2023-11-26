1bd0519f5605f0751ecf4b67a578e0248538d62b
enabledornot <102371948+enabledornot@users.noreply.github.com>
2023-11-25 21:28:12 -0500

    added multithreaded solution

--- problem_7/problem_7.go
+++ problem_7/problem_7.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func checkPrime(prevPrimes *[]int, possiblePrime int) bool {
@@ -25,7 +26,42 @@ func findPrimes(x int) []int {
 	return prevPrimes
 }
 
+func checkPrimeWg(prevPrimes *[]int, possiblePrime int, output chan int) {
+	// defer wg.Done()
+	if checkPrime(prevPrimes,possiblePrime) {
+		output <- possiblePrime
+	} else {
+		output <- (-1)*possiblePrime
+	}
+}
+
+func findPrimesThreaded(x int) []int {
+	prevPrimes := make([]int, 0, x)
+	currentNumber := 2
+	for x > len(prevPrimes) {
+		squareBound := currentNumber^2
+		newPrimes := make([]int, 0, x)
+		newprimechan := make(chan int)
+		count := 0
+		for x > len(prevPrimes) && squareBound > len(prevPrimes) {
+			go checkPrimeWg(&prevPrimes, currentNumber, newprimechan)
+			currentNumber+=1
+			count+=1
+		}
+		for i := 0; i < count ; i++ {
+			np := <- newprimechan
+			if np > 0 {
+				newPrimes = append(newPrimes,np)
+			}
+		}
+		sort.Ints(newPrimes)
+		prevPrimes = append(prevPrimes,newPrimes...)
+	}
+	return prevPrimes
+}
+
 func main() {
-	primes := findPrimes(100)
+	primes := findPrimes(10001)
+	fmt.Println("Length of Primes: ",len(primes))
 	fmt.Println("Primes: ",primes)
 }
